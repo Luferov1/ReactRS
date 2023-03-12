a467ed5dc8e04dc470ba5bcbb7050efe4dbab3b7
Luferov1 <ldima12022002@gmail.com>
2023-03-12 18:45:01 +0300

    feat: add fonts

--- components/src/main.tsx
+++ components/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
+import './fonts/Poppins-Light.ttf';
+import './fonts/Poppins-Medium.ttf';
+import './fonts/Poppins-Bold.ttf';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
