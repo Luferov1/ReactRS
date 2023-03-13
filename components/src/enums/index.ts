926698f475a2658686da63c8cebf4f78b7ce735a
Luferov1 <ldima12022002@gmail.com>
2023-03-13 19:58:27 +0300

    feat: add new enums

--- components/src/enums/index.ts
+++ components/src/enums/index.ts
@@ -8,3 +8,24 @@ export enum FooterLinks {
   RsSchool = 'https://rs.school/',
   GH = 'https://github.com/Luferov1',
 }
+
+export enum TeamNames {
+  MUN = 'Manchester United',
+  MCI = 'Manchester City',
+  ARS = 'Arsenal',
+  BHA = 'Brighton & Hove Albion',
+  BRE = 'Brentford',
+  BOU = 'Bournemouth',
+  NEW = 'Newcastle',
+  TOT = 'Spurs',
+  LEI = 'Leister',
+  LIV = 'Liverpool',
+  AVL = 'Aston Villa',
+}
+
+export enum Positions {
+  GK = 'GoalKeeper',
+  DEF = 'Defender',
+  MID = 'Midfielder',
+  FWD = 'Forward',
+}
