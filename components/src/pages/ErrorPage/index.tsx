4225a496f80b56071d09fca6136899c28961a5d8
Luferov1 <ldima12022002@gmail.com>
2023-03-15 11:51:10 +0300

    feat: modified error page

--- components/src/pages/ErrorPage/index.tsx
+++ components/src/pages/ErrorPage/index.tsx
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './style.module.scss';
+import Button from '../../components/Button';
+import { Path } from '../../enums';
 
-class ErrorPage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-      </div>
-    );
-  }
-}
+const ErrorPage = () => (
+  <div className={styles.container}>
+    <p className={styles.message}>
+      Oops! Something went wrong. This page was deleted or doesn&apos;t exist
+    </p>
+    <Link to={Path.Home}>
+      <Button width={100} height={60} text="Go back home" />
+    </Link>
+  </div>
+);
 
 export default ErrorPage;
