b175a482cf773fc92b396b27d8c9b91e0f5b30c3
Luferov1 <ldima12022002@gmail.com>
2023-03-18 15:23:11 +0300

    feat: add some scripts

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -5,13 +5,22 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude, 'src/main.tsx'],
+      all: true,
+      src: ['src'],
+      provider: 'c8',
+      reporter: ['text'],
+    },
   },
 });
