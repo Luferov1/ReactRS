e0c335cce709f1a669c2bc4e62921a8292cb6374
Luferov1 <ldima12022002@gmail.com>
2023-03-25 17:37:18 +0300

    refactor: change data

--- components/src/constants/playersInfo/index.ts
+++ components/src/constants/playersInfo/index.ts
@@ -3,7 +3,7 @@ import { Positions, TeamNames } from '../../enums';
 export interface IPlayer {
   name: string;
   team: TeamNames;
-  form: number;
+  birthDate: string;
   price: number;
   selected: number;
   position: Positions;
@@ -15,7 +15,7 @@ const players: IPlayer[] = [
   {
     name: 'David Raya',
     team: TeamNames.BRE,
-    form: 2.8,
+    birthDate: '15.09.1995',
     price: 4.8,
     selected: 15.6,
     position: Positions.GK,
@@ -25,7 +25,7 @@ const players: IPlayer[] = [
   {
     name: 'Nick Pope',
     team: TeamNames.NEW,
-    form: 1.0,
+    birthDate: '19.04.1992',
     price: 5.4,
     selected: 20.9,
     position: Positions.GK,
@@ -35,7 +35,7 @@ const players: IPlayer[] = [
   {
     name: 'Kieran Trippier',
     team: TeamNames.NEW,
-    form: 2.2,
+    birthDate: '19.09.1990',
     price: 6.0,
     selected: 64.9,
     position: Positions.DEF,
@@ -45,7 +45,7 @@ const players: IPlayer[] = [
   {
     name: 'Oleksandr Zinchenko',
     team: TeamNames.ARS,
-    form: 5.5,
+    birthDate: '15.12.1996',
     price: 5.2,
     selected: 14.5,
     position: Positions.DEF,
@@ -55,7 +55,7 @@ const players: IPlayer[] = [
   {
     name: 'Pervis Estupiñán',
     team: TeamNames.BHA,
-    form: 3.8,
+    birthDate: '21.01.1998',
     price: 4.8,
     selected: 11.7,
     position: Positions.DEF,
@@ -65,7 +65,7 @@ const players: IPlayer[] = [
   {
     name: 'Pedro Porro',
     team: TeamNames.TOT,
-    form: 3.0,
+    birthDate: '13.09.1999',
     price: 4.8,
     selected: 0.3,
     position: Positions.DEF,
@@ -75,7 +75,7 @@ const players: IPlayer[] = [
   {
     name: 'Lewis Dunk',
     team: TeamNames.BHA,
-    form: 2.2,
+    birthDate: '21.11.1991',
     price: 4.7,
     selected: 5.1,
     position: Positions.DEF,
@@ -85,7 +85,7 @@ const players: IPlayer[] = [
   {
     name: 'Kaoru Mitoma',
     team: TeamNames.BHA,
-    form: 4.6,
+    birthDate: '20.05.1997',
     price: 5.5,
     selected: 22.4,
     position: Positions.MID,
@@ -95,7 +95,7 @@ const players: IPlayer[] = [
   {
     name: 'Gabriel Martinelli',
     team: TeamNames.ARS,
-    form: 7.3,
+    birthDate: '18.06.2001',
     price: 6.6,
     selected: 37.5,
     position: Positions.MID,
@@ -105,7 +105,7 @@ const players: IPlayer[] = [
   {
     name: 'James Maddison',
     team: TeamNames.LEI,
-    form: 1.2,
+    birthDate: '23.11.1996',
     price: 8.1,
     selected: 5.8,
     position: Positions.MID,
@@ -115,7 +115,7 @@ const players: IPlayer[] = [
   {
     name: 'Mohamed Salah',
     team: TeamNames.LIV,
-    form: 8.2,
+    birthDate: '15.06.1992',
     price: 12.8,
     selected: 28.0,
     position: Positions.MID,
@@ -125,7 +125,7 @@ const players: IPlayer[] = [
   {
     name: 'Marcus Rashford',
     team: TeamNames.MUN,
-    form: 7.0,
+    birthDate: '31.10.1997',
     price: 7.4,
     selected: 53.8,
     position: Positions.MID,
@@ -135,7 +135,7 @@ const players: IPlayer[] = [
   {
     name: 'Erling Haaland',
     team: TeamNames.MCI,
-    form: 5.3,
+    birthDate: '21.07.2000',
     price: 12.2,
     selected: 82.3,
     position: Positions.FWD,
@@ -145,7 +145,7 @@ const players: IPlayer[] = [
   {
     name: 'Ivan Toney',
     team: TeamNames.BRE,
-    form: 3.0,
+    birthDate: '16.03.1996',
     price: 7.8,
     selected: 39.6,
     position: Positions.FWD,
@@ -155,7 +155,7 @@ const players: IPlayer[] = [
   {
     name: 'Ollie Watkins',
     team: TeamNames.AVL,
-    form: 6.2,
+    birthDate: '30.12.1995',
     price: 7.3,
     selected: 9.5,
     position: Positions.FWD,
@@ -165,7 +165,7 @@ const players: IPlayer[] = [
   {
     name: 'Harry Kane',
     team: TeamNames.TOT,
-    form: 6.5,
+    birthDate: '28.07.1993',
     price: 11.7,
     selected: 38.5,
     position: Positions.FWD,
