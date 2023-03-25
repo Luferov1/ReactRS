50d2755d7531c0b3cecf777858fb668604d90ee3
Luferov1 <ldima12022002@gmail.com>
2023-03-25 17:39:47 +0300

    feat: add submitted message component

---
+++ components/src/pages/FormPage/Form/SubmittedMessage/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+const SubmittedMessage = () => (
+  <div className={styles.submittedMessage}>Card has been saved</div>
+);
+
+export default SubmittedMessage;
