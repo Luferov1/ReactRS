bb5a4d7920c591cbe006e5ef77b2638355dc65d3
Luferov1 <ldima12022002@gmail.com>
2023-04-14 20:47:02 +0300

    refactor: refactor root page

--- components/src/pages/RootPage/index.tsx
+++ components/src/pages/RootPage/index.tsx
@@ -3,16 +3,41 @@ import { Outlet } from 'react-router-dom';
 import styles from './style.module.scss';
 import Header from './Header';
 import Footer from './Footer';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { mainPageSlice } from '../../store/reducers/MainPageSlice';
 
-const RootPage = () => (
-  <div className={styles.wrapper}>
-    <Header />
-    <div className={styles.background}>
-      <div className={styles.players} />
+const RootPage = () => {
+  const { isModalOpened } = useAppSelector((state) => state.mainPageReducer);
+  const dispatch = useAppDispatch();
+  const { setIsModalOpened } = mainPageSlice.actions;
+  return (
+    <div
+      className={
+        isModalOpened
+          ? [styles.wrapper, styles.hidden].join(' ')
+          : styles.wrapper
+      }
+      aria-hidden="true"
+      onClick={() => {
+        if (isModalOpened) {
+          dispatch(setIsModalOpened(false));
+        }
+      }}
+    >
+      <Header />
+      <div
+        className={
+          isModalOpened
+            ? [styles.background, styles.hiddenBackground].join(' ')
+            : styles.background
+        }
+      >
+        <div className={styles.players} />
+      </div>
+      <Outlet />
+      <Footer />
     </div>
-    <Outlet />
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default RootPage;
