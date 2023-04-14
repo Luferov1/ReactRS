dac724763a5a6da539bd2e39436c72826f982f92
Luferov1 <ldima12022002@gmail.com>
2023-04-14 20:43:18 +0300

    refactor: refactor modal window container

--- components/src/components/ModalWindowContainer/index.tsx
+++ components/src/components/ModalWindowContainer/index.tsx
@@ -1,80 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './style.module.scss';
 import LoadingIndicator from '../LoadingIndicator';
-import { FullPlayerInfo, ModalInitialState } from '../../interfaces';
-import { fetchPlayerById } from '../../functions';
 import ModalWindow from './ModalWindow';
+import { useAppSelector } from '../../hooks';
+import playersAPI from '../../services';
 
-interface Props extends ModalInitialState {
-  setModalInitState: React.Dispatch<
-    React.SetStateAction<ModalInitialState | null>
-  >;
-}
-
-const ModalWindowContainer = ({
-  id,
-  setModalInitState,
-  team,
-  numberOfGoals,
-}: Props) => {
-  const [isLoading, setLoading] = useState(true);
-  const [state, setState] = useState<FullPlayerInfo | null>(null);
-  useEffect(() => {
-    fetchPlayerById(id, setLoading).then((data) => {
-      if (data) {
-        setState({
-          team,
-          id,
-          nationality: data.nationality,
-          name: data.name,
-          position: data.position,
-          numberOfGoals,
-          dateOfBirth: data.dateOfBirth,
-        });
-      }
-    });
-  }, []);
+const ModalWindowContainer = () => {
+  const { modalInitState } = useAppSelector(
+    (rootState) => rootState.mainPageReducer
+  );
+  const { team, id, numberOfGoals } = modalInitState;
+  const {
+    data: info,
+    isLoading,
+    isError,
+  } = playersAPI.useFetchPlayerByIdQuery(id);
 
   const handleClick = (event: React.MouseEvent) => {
     event.stopPropagation();
   };
 
   return (
-    <button
-      className={styles.background}
-      onClick={() => setModalInitState(null)}
-      type="button"
-    >
-      {(() => {
-        if (isLoading) {
-          return <LoadingIndicator />;
-        }
-        if (!state) {
-          return (
-            <button
-              className={styles.serverError}
-              onClick={handleClick}
-              type="button"
-            >
-              Server error. You have exceeded the allowed number of requests per
-              minute. Please wait and try again
-            </button>
-          );
-        }
-        return (
-          <ModalWindow
-            handleClick={handleClick}
-            team={state.team}
-            numberOfGoals={state.numberOfGoals}
-            id={state.id}
-            dateOfBirth={state.dateOfBirth}
-            nationality={state.nationality}
-            position={state.position}
-            name={state.name}
-          />
-        );
-      })()}
-    </button>
+    <>
+      {isLoading && <LoadingIndicator />}
+      {isError && (
+        <button
+          className={styles.serverError}
+          onClick={handleClick}
+          type="button"
+        >
+          Server error. You have exceeded the allowed number of requests per
+          minute. Please wait and try again
+        </button>
+      )}
+      {info && (
+        <ModalWindow
+          handleClick={handleClick}
+          team={team}
+          numberOfGoals={numberOfGoals}
+          id={id}
+          dateOfBirth={info.dateOfBirth}
+          nationality={info.nationality}
+          position={info.position}
+          name={info.name}
+        />
+      )}
+    </>
   );
 };
 
