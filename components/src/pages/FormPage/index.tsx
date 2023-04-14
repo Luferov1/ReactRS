cdb135da44d0627847ea0cbb5337da8a4043b3cb
Luferov1 <ldima12022002@gmail.com>
2023-04-14 20:44:22 +0300

    refactor: refactor form page

--- components/src/pages/FormPage/index.tsx
+++ components/src/pages/FormPage/index.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './style.module.scss';
 import Form from './Form';
-import { IPlayer } from '../../constants/playersInfo';
 import PlayerCard from '../../components/PlayerCard';
+import { useAppSelector } from '../../hooks';
+import { generateUniqueId } from '../../functions';
 
 const FormPage = () => {
-  const [playersArr, setPlayers] = useState<IPlayer[]>([]);
+  const { newCards } = useAppSelector((state) => state.formPageReducer);
   return (
     <main className={styles.main}>
       <h2 className={styles.header}>Create new player card</h2>
-      <Form setPlayers={setPlayers} players={playersArr} />
+      <Form />
       <div className={styles.cardsContainer}>
-        {playersArr.map(
-          ({ id, img, team, position, selected, name, birthDate, price }) => (
+        {newCards.map(
+          ({ img, team, position, selected, name, birthDate, price }) => (
             <PlayerCard
               name={name}
               team={team}
@@ -21,8 +22,7 @@ const FormPage = () => {
               selected={selected}
               position={position}
               img={img}
-              id={id}
-              key={id}
+              key={generateUniqueId(name)}
             />
           )
         )}
