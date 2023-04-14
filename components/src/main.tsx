93a4493f07e459d1f717a8f17f8bd0fe046b7dcd
Luferov1 <ldima12022002@gmail.com>
2023-04-14 20:39:18 +0300

    feat: add provider

--- components/src/main.tsx
+++ components/src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import router from './Router';
 import './fonts/Poppins-Light.ttf';
 import './fonts/Poppins-Medium.ttf';
 import './fonts/Poppins-Bold.ttf';
+import { setupStore } from './store';
+
+const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
